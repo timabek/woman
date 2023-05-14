d9ce9c6e8017612a4386834a0b7a83f35dcb7a7c
timaDev00 <timabeknarynbekovich@gmail.com>
2023-05-14 17:10:31 +0600

    add it

--- client/src/canvas/index.jsx
+++ client/src/canvas/index.jsx
@@ -1,29 +1,58 @@
 import { Canvas } from '@react-three/fiber'
 import { Environment, Center } from '@react-three/drei';
+import { useState } from 'react';
+import html2canvas from 'html2canvas';
 
 import Shirt from './Shirt';
 import Backdrop from './Backdrop';
 import CameraRig from './CameraRig';
 
 const CanvasModel = () => {
-  return (
-    <Canvas
-      shadows
-      camera={{ position: [0, 0, 0], fov: 25 }}
-      gl={{ preserveDrawingBuffer: true }}
-      className="w-full max-w-full h-full transition-all ease-in"
-    >
-      <ambientLight intensity={0.5} />
-      <Environment preset="city" />
+  const [isDownloading, setIsDownloading] = useState(false);
 
-      <CameraRig>
-        <Backdrop />
-        <Center>
-          <Shirt />
-        </Center>
-      </CameraRig>
-    </Canvas>
+  const handleDownloadClick = () => {
+    setIsDownloading(true);
+    const canvas = document.querySelector('.canvas-container canvas');
+    html2canvas(canvas, {
+      scale: 5 // double the size of the canvas
+    }).then((canvas) => {
+      const link = document.createElement('a');
+      link.download = 'dress.png';
+      link.href = canvas.toDataURL();
+      link.click();
+      setIsDownloading(true);
+    });
+  }
+  
+
+  return (
+    <div className="canvas-container w-full h-full ">
+      {isDownloading && (
+        <div className="absolute top-0 left-0 w-full h-full flex items-center justify-center">
+          <span className="text-white">Downloading...</span>
+        </div>
+      )}
+      <Canvas
+        shadows
+        camera={{ position: [0, 0, 0], fov: 25 }}
+        gl={{ preserveDrawingBuffer: true }}
+        className="w-full max-w-full h-full transition-all ease-in"
+      >
+        <ambientLight intensity={0.5} />
+        <Environment preset="city" />
+
+        <CameraRig>
+          <Backdrop />
+          <Center>
+            <Shirt />
+          </Center>
+        </CameraRig>
+      </Canvas>
+      <button onClick={handleDownloadClick} className="absolute bottom-1 right-4 p-2 bg-[#8c0303] text-white font-bold rounded">
+        Download
+      </button>
+    </div>
   )
 }
 
-export default CanvasModel
+export default CanvasModel;
